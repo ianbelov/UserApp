4903aee3f87d62898fc611253edba69659302caa
Ivan Belov <kaithearchitect.coding@gmail.com>
2020-06-27 15:23:46 +0300

    move UserAppApplicationIT to integration-test phase

---
+++ app/src/test/java/com/ian/app/UserAppApplicationIT.java
@@ -0,0 +1,19 @@
+package com.ian.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ComponentScan("com.ian")
+
+public class UserAppApplicationIT {
+
+    @Test
+    public void contextLoads() {
+        UserAppApplication.main(new String[]{});
+    }
+}
